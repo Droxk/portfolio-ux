f6f28abef996cc5cfd8e838250fd00f3bde0207f
Droxk <dvillaverdeprado@gmail.com>
2021-11-04 16:27:45 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// let projects_list = document.getElementById("projects");
+
+// function setOpacity(elem, opac) {
+//     elem.style.opacity = opac;
+// }
+
+// // Asignacion de eventos
+// projects_list.addEventListener('mouseover', function (event) {
+
+// 	if (event.target.matches('.project')) {
+//         setOpacity(event.target, '0.3');
+// 	}
+
+// }, false);
+
+// projects_list.addEventListener('mouseout', function (event) {
+
+// 	if (event.target.matches('.project')) {
+//         setOpacity(event.target, '1');
+// 	}
+
+// }, false);
