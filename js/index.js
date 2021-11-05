7867e2099da9a208f4d083304116eba026c71c03
Droxk <dvillaverdeprado@gmail.com>
2021-11-05 15:21:52 +0100

    Advancing on js effects and content

--- js/index.js
+++ js/index.js
@@ -1,22 +1,61 @@
-// let projects_list = document.getElementById("projects");
+var navigation = document.getElementById("navigation");
+var projects_grid = document.getElementById("projects");
+var about_page = document.getElementById("about");
+var contact_page = document.getElementById("contact");
 
-// function setOpacity(elem, opac) {
-//     elem.style.opacity = opac;
-// }
+var projects_divs = document.querySelectorAll(".project");
 
-// // Asignacion de eventos
-// projects_list.addEventListener('mouseover', function (event) {
+function resize_grid(elements) {
+    elements.forEach(element => {
+        width = element.offsetWidth + "px";
+        element.style.height = width;
+    });
+}
 
-// 	if (event.target.matches('.project')) {
-//         setOpacity(event.target, '0.3');
-// 	}
+resize_grid(projects_divs);
 
-// }, false);
+visualViewport.onresize = function() {
+    resize_grid(projects_divs);
+};
 
-// projects_list.addEventListener('mouseout', function (event) {
+// Asignacion de eventos
+navigation.addEventListener('click', function (event) {
 
-// 	if (event.target.matches('.project')) {
-//         setOpacity(event.target, '1');
-// 	}
+    if (event.target.matches('#work_link')) {
+        projects_grid.scrollIntoView({ behavior: 'smooth', block: 'end'});
+    }
+    if (event.target.matches('#about_link')) {
+        about_page.scrollIntoView({ behavior: 'smooth', block: 'end'});
+    }
+    if (event.target.matches('#contact_link')) {
+        contact_page.scrollIntoView({ behavior: 'smooth', block: 'end'});
+    }
 
-// }, false);
+}, false);
+
+projects_grid.addEventListener('mouseover', function (event) {
+
+    if (event.target.matches('.stretched-link')) {
+        event.target.parentElement.querySelector('.overlay').classList.add('show');
+        event.target.parentElement.querySelector('.project-heading').classList.add('show');
+    }
+
+}, false);
+
+projects_grid.addEventListener('mouseout', function (event) {
+
+    if (event.target.matches('.stretched-link')) {
+        event.target.parentElement.querySelector('.overlay').classList.remove('show');
+        event.target.parentElement.querySelector('.project-heading').classList.remove('show');
+    }
+
+}, false);
+
+window.addEventListener('scroll', function(e) {
+    if(window.scrollY <= 160)  {
+        navigation.classList.remove('shadow');
+    };
+    if(window.scrollY > 160)  {
+        navigation.classList.add('shadow');
+    };
+});
